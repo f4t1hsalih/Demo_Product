7a6e3a402a5e9a9a4b2226732dae518b5ae6bf3b
f4t1hsalih <f4t1hsalih@gmail.com>
2024-01-13 12:19:25 +0300

    Ders 50 - Job Tablosu ve İlişkiler

--- EntityLayer/Concrete/Customer.cs
+++ EntityLayer/Concrete/Customer.cs
@@ -5,5 +5,9 @@
         public int ID { get; set; }
         public string? Name { get; set; }
         public string? City { get; set; }
+
+        //İlişki kurmak için gerekli olan kodlar
+        public int JobID { get; set; }
+        public Job Job { get; set; }
     }
 }
---
+++ EntityLayer/Concrete/Job.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Job
+    {
+        public int JobID { get; set; }
+        public string? Name { get; set; }
+
+        //İlişki kurmak için gerekli olan kodlar
+        public List<Customer> Customers { get; set; }
+    }
+}
