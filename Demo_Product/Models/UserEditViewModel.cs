b3c0b2c5406bdb9ea9ccf08e3a3903f116f50bf7
f4t1hsalih <f4t1hsalih@gmail.com>
2024-01-18 22:46:04 +0300

    Ders 71 - Settings Sayfası

---
+++ Demo_Product/Models/UserEditViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo_Product.Models
+{
+    public class UserEditViewModel
+    {
+        [Required(ErrorMessage = "Lütfen bir isim giriniz!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Lütfen bir soyisim giriniz!")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Lütfen cinsiyet giriniz!")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Lütfen bir şifre giriniz!")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Lütfen bir maail adresi giriniz!")]
+        public string Mail { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifreyi tekrar giriniz!")]
+        [Compare("Password", ErrorMessage = "Lütfen şifrelerin eşleştiğinden emin olun")]
+        public string ConfirmPassword { get; set; }
+    }
+}
--- Demo_Product/Program.cs
+++ Demo_Product/Program.cs
@@ -29,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Product}/{action=Index}/{id?}");
+    pattern: "{controller=Login}/{action=Index}/{id?}");
 
 app.Run();
